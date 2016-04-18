f05e96a9bc283fc59f2e0bf7a5c041f98d739cfc
Michael <mikeshasaco@gmail.com>
2016-04-18 15:26:58 -0400

    add association between user and artilces

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ActiveRecord::Base
+	belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   #title of article cant be blank, length of article
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
   #my give a description to describe, length of description
   #terminal ->rails console article.new(title: "new")
-  
+  validates :user_id, presence: true
 end
---
+++ db/migrate/20160418044025_add_id_articles.rb
@@ -0,0 +1,5 @@
+class AddIdArticles < ActiveRecord::Migration
+  def change
+  	
+  end
+end
