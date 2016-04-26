b4bb7686d8032dc389f2f5a4b9bfebc960b6c9a9
Michael <mikeshasaco@gmail.com>
2016-04-26 19:56:45 -0400

    functionality and admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418195540) do
+ActiveRecord::Schema.define(version: 20160426232139) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160418195540) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
